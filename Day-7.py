0b42206fbdabfa0b1e7d85e6ea874b2f9054d19d
Suphal <suphalbochkar@gmail.com>
2022-11-24 23:50:17 +0530

    Celcius to Farenheit

--- Day-7.py
+++ Day-7.py
@@ -5,7 +5,10 @@
 # s=int(input())
 # if 1<=d<=30 and 1<=s<=6000:
 #     print(f'{(d*s):.2f}')
-
+# ?Celcius to Farenheit
+# c=float(input())
+# if 1.00<=c<=57.00:
+#     print(f'{((c*(9/5))+32):.2f}')
 
 
 
