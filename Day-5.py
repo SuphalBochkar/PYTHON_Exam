684560195c77cc47de11248448ed2190f75cec4e
Suphal <suphalbochkar@gmail.com>
2022-11-24 23:45:15 +0530

    Reverse By spaces

---
+++ Day-5.py
@@ -0,0 +1,17 @@
+
+#^ Arithmetic Operators
+#? print reverse by spaces
+# a=int(input())
+# b=str(a)
+# !M-1   No of digits not mentioned
+# i=0
+# while a>0:
+#     r=a%10
+#     i=(i*10)+r
+#     a=a//10
+# for i in range (len(b)-1,-1,-1):
+#     print(b[i],end=" ")
+
+
+
+
