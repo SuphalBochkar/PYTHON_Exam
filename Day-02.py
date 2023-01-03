8fe67f09a5d31d28ab340704ef5b4e168f2742a9
Suphal <suphalbochkar@gmail.com>
2023-01-03 14:13:13 +0530

    Update Day-02.py

--- Day-02.py
+++ Day-02.py
@@ -1,3 +1,18 @@
+
+#^ Add,Substitute
+# ? Substituting
+    # x="pi"
+    # y=3.414
+    # print(x,"value is",y)
+#? The only Last assigined value includes
+    # x=float(0.5)
+    # y=float(0.7)
+    # x=10
+    # y=20
+    # print(x*y)
+#? Area of circle
+    # R=int(input("R: "))
+    # print(3.141*(R**2))
 # ?Addition works on string Expression
     # x="Hii"
     # y="Bro"
