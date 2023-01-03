05f23341e0d37396d4ba5ddaec217a476b6aadc3
Suphal <suphalbochkar@gmail.com>
2023-01-03 14:13:09 +0530

    Create Day-02.py

---
+++ Day-02.py
@@ -0,0 +1,16 @@
+# ?Addition works on string Expression
+    # x="Hii"
+    # y="Bro"
+    # print(x+y)
+#? Statement
+    # p=50.5
+    # print(p)
+    # q=10
+    # q=q*20    #! We can Use for printing multiple Times
+    # print(p+q)
+# ? Equality
+    # a=33
+    # if a%3 ==0:          #! For Equals use ==
+    #     print("Divisible by 3")
+
+
