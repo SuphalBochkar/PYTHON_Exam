342a5c0252c93d28b23e61375e3f9d3a456fb910
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-05 21:54:16 +0530

    Create Day-04.py

---
+++ Day-04.py
@@ -0,0 +1,12 @@
+
+#^ Converting into types
+# ? Changing into string and int
+    # d=float(input("Number: "))
+    # print(int(d))
+    # print(str(d))
+    
+# ?Space as String
+    # print("a b c d e")
+    # print("a%b%c%d%e")
+
+# ?Substituting
