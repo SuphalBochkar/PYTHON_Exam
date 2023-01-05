8d5d2f25974a81967e76d9e79abb8198ea0dc666
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-05 21:54:35 +0530

    Update Day-04.py

--- Day-04.py
+++ Day-04.py
@@ -10,3 +10,28 @@
     # print("a%b%c%d%e")
 
 # ?Substituting
+    # p=float(input("P: "))
+    # q=int(input("Q: "))
+    # r=p*q
+    # print(f'p={p:.6f},q={q},product{r:.6f}')
+
+# ?Binary And Octal
+    # x=int(input())
+    # y=int(input())
+    # a=format(x,'b')
+    # b=format(x,'o')
+    # print(f'{a},{b}')
+
+# ?Spacing in comma and format
+    # n=input()
+    # print("  ",n)                        #!Comma Prints Extra Space
+    # print("     ",n)
+    # print(f'  {n}')                       #!format prints exact
+    # print(f'    {n}')
+    
+# ?Order and unicode ASCII
+    # c=input()
+    # o=ord(c)
+    # print(o)
+    # print(chr(o*2))
+        
