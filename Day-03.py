9698e5ae0d85afdfa3e5a44886cc7c015e57c4ae
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-06 15:35:20 +0530

    Create Day-03.py

---
+++ Day-03.py
@@ -0,0 +1,18 @@
+
+#^ Data Types :- Num,Strings,list, tuple, Sets
+#? Printing Types  of Function
+    # x="Friction"
+    # y=123
+    # z=8.2
+    # a=3+4j
+    # print(type(x))
+    # print(type(y))
+    # print(type(z))
+    # print(type(a))
+#? Printing Odd Index 
+    # A="COMMUNICATION"
+    # for i in range (len(A)):     
+    #     if i%2!=0:
+    #         print(A[i])
+#? Lists
+    # a="globalwarming"
