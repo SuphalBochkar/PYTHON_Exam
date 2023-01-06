0cc52f8addbce137029c47e0ddd24992a96f0020
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-06 15:35:42 +0530

    V-2

---
+++ Day-07.py
@@ -0,0 +1,29 @@
+
+# ^Limits
+#? Limiting The input
+# d=int(input())
+# s=int(input())
+# if 1<=d<=30 and 1<=s<=6000:
+#     print(f'{(d*s):.2f}')
+# ?Celcius to Farenheit
+# c=float(input())
+# if 1.00<=c<=57.00:
+#     print(f'{((c*(9/5))+32):.2f}')
+# ?Swap Last and First
+# s=input()
+# !M-1     Length limit (Eg:5)
+# print(f'{s[-1]}{s[1]}{s[2]}{s[3]}{s[0]}')
+# !M-2
+# b=""
+# for i in range (1,len(s)-2):
+#     b+= s[i]
+# print(s[-1]+b+s[0])
+# !M-3
+# ?Years Months Days
+# days=int(input())
+# years=days//365
+# remain_days=days%365
+# months=remain_days//30
+# remain_days1=remain_days%30
+# print(f'Y:{years} M:{months} D:{remain_days1}')
+
