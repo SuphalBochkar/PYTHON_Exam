f1d5523e94571068964ab7ff4b4925f23053207a
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-06 15:35:22 +0530

    Update Day-03.py

--- Day-03.py
+++ Day-03.py
@@ -16,3 +16,19 @@
     #         print(A[i])
 #? Lists
     # a="globalwarming"
+    # b=(f'{a[0]}{a[1]}{a[2]}')
+    # print(b)
+    # print((a+b)*2)
+#? Printing Letters in a String.
+    # a="programming"
+    # for i in [0,1,2,-3,-2,-1]:
+    #     print(a[i])
+#? Math on Complex
+    # a=2+3j
+    # b=3+4j
+    # print(a+b)
+    # print(a*b)
+#? Making A complex Number
+    # a=complex(3,5)
+    # print(a)
+
