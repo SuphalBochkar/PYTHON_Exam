0863dcec0a7d1c8fae08f9480de57050218eabdf
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-06 15:35:26 +0530

    Create Day-05.py

---
+++ Day-05.py
@@ -0,0 +1,23 @@
+a = 45645454
+#^ Arithmetic Operators
+#? print reverse by spaces
+    # a=int(input())
+    # b=str(a)
+    # !M-1   No of digits not mentioned
+    # i=0
+    # while a>0:
+    #     r=a%10
+    #     i=(i*10)+r
+    #     a=a//10
+    # for i in range (len(b)-1,-1,-1):
+    #     print(b[i],end=" ")
+    # !M-2   limit is given (Eg:3)
+    # if 99<a<1000:
+    #     print(b[-1],b[-2],b[-3])
+    # !M-3
+for i in range (len(str(a))-1,-1,-1):
+    a = str(a)
+    print(a[i],end=" ")
+
+
+
