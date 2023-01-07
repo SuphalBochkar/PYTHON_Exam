efb0a15a3b262de858d955eea8cfd3eeb65f9e29
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-07 15:37:02 +0530

    Update Day-09.py

--- Day-09.py
+++ Day-09.py
@@ -12,3 +12,27 @@
 # s = input()
 # for i in s :
 #     print(i*2,end="")
+
+# ?Amgstrong number
+# n = input()
+# l = len(n)
+# a = 0
+# for i in n:
+#     a += int(i)**l
+# print(a == int(n))
+
+#? Profit or Loss 
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print(a-b)
+# else:
+#     print(b-a)
+    
+# ?Compound interest
+# p = int(input())
+# t = int(input())
+# r = float(input())
+# print(f'CI is {(p*((1+(r/12))**(12*t)))-p:.2f}')
+
+
