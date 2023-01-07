2524ae5427bb0f23d37940f60abd6c4a752c2870
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-07 15:36:59 +0530

    Create Day-09.py

---
+++ Day-09.py
@@ -0,0 +1,14 @@
+
+# ?Bool of reverse
+# s = input()
+# print(s == s[::-1])
+
+# ?inserting between the string
+# s = input()
+# for i in s :
+#     print(i,end="#")
+
+#? print intex by multiplying
+# s = input()
+# for i in s :
+#     print(i*2,end="")
