0382295dc837624f7d1305c6a958e926d70e6573
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-07 15:36:49 +0530

    Create Day-08.py

---
+++ Day-08.py
@@ -0,0 +1,12 @@
+
+#? N naturan numbers
+# b = int(input())
+# print(f'{(2*b*(b+1)*((b*2)+1)/3):.2f}')
+
+#? Divide power by 4 and 2 decimals
+# print(f'{(int(input())**(1/4)):.2f}')
+
+#? // Division
+# print(int(input())//10)
+
+#? // for 2 inputs
