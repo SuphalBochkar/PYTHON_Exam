73d70ef6a35ce76d33582bc9e02c171796308605
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-08 23:31:50 +0530

    String

--- Day-10.py
+++ Day-10.py
@@ -1,33 +1,39 @@
-# Negative Numbers
+
+#? Negative Numbers
 #     i=int(input())
 #     j=int(input())
 #     if i<0 and j<0 :
 #         print("True")
 #     else :
 #         print("False")
-# Even Or Odd Checker
+
+#? Even Or Odd Checker
 #     n=int(input())
 #     if (n%2) == 0:
 #         print("True")
 #     else :
 #         print("False")
-# String Checker
+
+#? String Checker
 #     x=str(input())
 #     y=str(input())
 #     if x[0]==y[0]:
 #         print("false")
 #     else :
 #         print("true")
-# String Checker 1
+
+#? String Checker 1
 #     a=str(input())
 #     b=str(input())
 #     print(a[-1] in b and a[-2] in b)
-# Large Number
+
+#? Large Number
 #     x=input()
 #     a=int(x[0])*int(x[-1])
 #     print(a)
 #     print(a%5 == 0)
-# Surface Area of Cylinder
+
+#? Surface Area of Cylinder
 #     r=int(input())
 #     h=int(input())
 #     area = (2*3.141*r*h) + 2*3.141*(r**2)
