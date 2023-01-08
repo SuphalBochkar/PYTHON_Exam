14069556a328831c676cef4344db8d94ecce83af
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-08 23:32:05 +0530

    Create Day-12.py

---
+++ Day-12.py
@@ -0,0 +1,17 @@
+
+#? Print Type 
+# a = 1600
+# b = 18.
+# c = 13-8j
+# d = "Levi"
+# e = True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+#? Complex Addition
+# a = 2 + 3j
+# b = 3 + 4j
+# print(a+b)
