d076a9f844c1ca965d09acdba071dcc8257f846a
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-08 23:32:08 +0530

    Update Day-12.py

--- Day-12.py
+++ Day-12.py
@@ -15,3 +15,19 @@
 # a = 2 + 3j
 # b = 3 + 4j
 # print(a+b)
+
+#? Int to Float & Int to hex
+# a = int(input("number: "))
+# print(float(a))
+# print(hex(a))
+
+#? Float to str & int
+# n = float(input())
+# print(str(n))
+# print(int(n))
+
+#? Int to Complex & octal
+# n = int(input())
+# print(complex(n))
+# print(oct(n))
+
