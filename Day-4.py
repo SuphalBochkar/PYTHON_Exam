f6465fa3d0582e3a28eef98fafd7d97af57a1201
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-21 21:16:51 +0530

    Delete Day-4.py

--- Day-4.py
+++ Day-4.py
@@ -14,18 +14,3 @@
 # print(f'p={p:.6f},q={q},product{r:.6f}')
 # ?Binary And Octal
 # x=int(input())
-# y=int(input())
-# a=format(x,'b')
-# b=format(x,'o')
-# print(f'{a},{b}')
-# ?Spacing in comma and format
-# n=input()
-# print("  ",n)                        #!Comma Prints Extra Space
-# print("     ",n)
-# print(f'  {n}')                       #!format prints exact
-# print(f'    {n}')
-# ?Order and unicode ASCII
-# c=input()
-# o=ord(c)
-# print(o)
-# print(chr(o*2))
