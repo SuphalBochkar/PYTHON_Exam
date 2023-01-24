a69a94c4b549eedcc2167a3008aaae736022cce3
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-24 22:14:55 +0530

    Update Day-13.py

--- Day-13.py
+++ Day-13.py
@@ -12,3 +12,18 @@
 # print(hex(p),complex(p,q))
 
 #? Locical And Not
+# p = int(input())
+# q = int(input())
+# c = p < q
+# b = p <= q
+# r = c and b
+# print(r)
+# print(not r)
+
+#? Volume Of cylinder
+# r = float(input())
+# h = float(input())
+# print(f'{3.14*(r**2)*h:.2f}')
+
+#? Assigment Operations
+# a = int(input())
