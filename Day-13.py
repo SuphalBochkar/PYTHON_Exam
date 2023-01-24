7d07cb6b0062cc8f4563cc432fbb0a133549b939
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-24 22:14:57 +0530

    Update Day-13.py

--- Day-13.py
+++ Day-13.py
@@ -27,3 +27,26 @@
 
 #? Assigment Operations
 # a = int(input())
+# b = int(input())
+# print(f'a+=b: a= {a+b} b= {b}')
+# print(f'a-=b: a= {a-b} b= {b}')
+# print(f'a*=b: a= {a*b} b= {b}')
+# print(f'a/=b: a= {b/a} b= {b}')
+# print(f'a**=b: a= {a**b} b= {b}')
+# print(f'a//=b: a= {a//b} b= {b}')
+# print(f'a%=b: a= {a%b} b= {b}')
+# print(f'a=b: a= {b} b= {b}')
+
+#? Expression SOlving 
+# a = int(input())`
+# b = int(input())
+# print(((a+b)**3)  >= ((a**4) + (b**2)))
+# print((a*b)//(a%b))`
+
+
+
+
+
+
+
+
