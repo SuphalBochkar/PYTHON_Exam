fad7f6a7ea248fe1b2626de3a4e605407c1e2b30
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-24 22:14:49 +0530

    Create Day-13.py

---
+++ Day-13.py
@@ -0,0 +1,14 @@
+
+#? Binary Octal
+# x = int(input())
+# y = int(input())
+# a = format(x,'b')
+# b = format(y,'o')
+# print(f'{a},{b}')
+
+#? int to hex & complex
+# p = int(input())
+# q = int(input())
+# print(hex(p),complex(p,q))
+
+#? Locical And Not
