a21905ce9251294ef6cf3d19b9ac69f719416184
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-24 22:15:04 +0530

    Delete Day-2.py

--- Day-2.py
+++ Day-2.py
@@ -1,19 +1,4 @@
-
 #^ Add,Substitute
-# ? Substituting
-    # x="pi"
-    # y=3.414
-    # print(x,"value is",y)
-#? The only Last assigined value includes
-    # x=float(0.5)
-    # y=float(0.7)
-    # x=10
-    # y=20
-    # print(x*y)
-#? Area of circle
-    # R=int(input("R: "))
-    # print(3.141*(R**2))
-# ?Addition works on string Expression
     # x="Hii"
     # y="Bro"
     # print(x+y)
@@ -22,10 +7,3 @@
     # print(p)
     # q=10
     # q=q*20    #! We can Use for printing multiple Times
-    # print(p+q)
-# ? Equality
-    # a=33
-    # if a%3 ==0:          #! For Equals use ==
-    #     print("Divisible by 3")
-
-
